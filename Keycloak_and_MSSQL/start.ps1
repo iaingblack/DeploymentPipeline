22cddce3970a3eb9359901448603947bc49a35d7
Iain Black <iain@rootisgod.xom>
2021-07-03 21:50:38 +0100

    Made a bit prettier

--- Keycloak_and_MSSQL/start.ps1
+++ Keycloak_and_MSSQL/start.ps1
@@ -1,6 +1,15 @@
-# https://github.com/docker/compose/issues/5222
-docker-compose -f "docker-compose.yml" up -d --build 2>&1 | %{ "$_" }
+# Build the containers
+Write-Host "-----------------------------------------------------"
+Write-Host "Building Containers"
+Write-Host "-----------------------------------------------------"
+docker build -f dockerfile-mssql-keycloak -t mssql-keycloak:latest .
 
+# Start Keycloak and MSSQL
+# https://github.com/docker/compose/issues/5222
+Write-Host "-----------------------------------------------------"
+Write-Host "Starting Docker Containers"
+Write-Host "-----------------------------------------------------"
+docker-compose -f "docker-compose.yml" up -d --build 2>&1 | %{ "$_" }
 $attempts = 10
 $repeat = $true
 while ($true) {
@@ -8,13 +17,19 @@ while ($true) {
     $alive = invoke-webrequest http://localhost:8080 -DisableKeepAlive -UseBasicParsing -ErrorAction SilentlyContinue
     if ($alive.StatusCode -eq '200') { break }
     else {
-        if ($attempts -gt 0) { Start-sleep 3; Write-Host "Waiting for Keycloak to start.. $attempts attempts remaining" }
+        if ($attempts -gt 0) { Start-sleep 5; Write-Host "Waiting for Keycloak to start.. $attempts attempts remaining" }
         else { throw "Exiting. Took too long" }
     }
 }
+Write-Host "-----------------------------------------------------"
 Write-Host "Keycloak Started"
+Write-Host "-----------------------------------------------------"
+Start-Sleep 2
 
-# Terraform Part
+# Terraform Config
+Write-Host "-----------------------------------------------------"
+Write-Host "Starting Terraform Configuration"
+Write-Host "-----------------------------------------------------"
 terraform init
 terraform plan
 
