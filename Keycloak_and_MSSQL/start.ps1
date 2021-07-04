b81d45f422d789d406734b10370fb20366bdda01
Iain Black <iain@rootisgod.xom>
2021-07-04 07:54:53 +0100

    Getting good. Need csv to create users from

--- Keycloak_and_MSSQL/start.ps1
+++ Keycloak_and_MSSQL/start.ps1
@@ -2,23 +2,43 @@
 Write-Host "-----------------------------------------------------"
 Write-Host "Building Containers"
 Write-Host "-----------------------------------------------------"
-docker build -f dockerfile-mssql-keycloak -t mssql-keycloak:latest .
+docker build -q -f dockerfile-mssql-keycloak -t mssql-keycloak:latest .
 
 # Start Keycloak and MSSQL
 # https://github.com/docker/compose/issues/5222
 Write-Host "-----------------------------------------------------"
-Write-Host "Starting Docker Containers"
+Write-Host "Checking if Docker Containers Are Running"
 Write-Host "-----------------------------------------------------"
-docker-compose -f "docker-compose.yml" up -d --build 2>&1 | %{ "$_" }
-$attempts = 10
-$repeat = $true
-while ($true) {
-    $attempts--
-    $alive = invoke-webrequest http://localhost:8080 -DisableKeepAlive -UseBasicParsing -ErrorAction SilentlyContinue
-    if ($alive.StatusCode -eq '200') { break }
-    else {
-        if ($attempts -gt 0) { Start-sleep 5; Write-Host "Waiting for Keycloak to start.. $attempts attempts remaining" }
-        else { throw "Exiting. Took too long" }
+$startContainers = $false
+try {
+    $alive =invoke-webrequest http://localhost:8080/auth/ -DisableKeepAlive -UseBasicParsing -ErrorAction SilentlyContinue
+}
+catch { 
+    Write-Host "Containers not started"
+    $startContainers = $true
+}
+if ($startContainers) {
+    Write-Host "-----------------------------------------------------"
+    Write-Host "Starting Docker Containers"
+    Write-Host "-----------------------------------------------------"
+    docker-compose -f "docker-compose.yml" up -d --build 2>&1 | %{ "$_" }
+    
+    $attempts = 10
+    while ($true) {
+        try {
+            $alive = invoke-webrequest http://localhost:8080/auth/ -DisableKeepAlive -UseBasicParsing -ErrorAction SilentlyContinue
+            break
+        }
+        catch { 
+            $attempts--
+            if ($attempts -gt 0) { 
+                Write-Host "Waiting for Keycloak to start.. $attempts attempts remaining"
+                Start-sleep 5
+            }
+            else { 
+                throw "Exiting. Took too long"
+            }
+        }
     }
 }
 Write-Host "-----------------------------------------------------"
@@ -31,6 +51,8 @@ Write-Host "-----------------------------------------------------"
 Write-Host "Starting Terraform Configuration"
 Write-Host "-----------------------------------------------------"
 terraform init
+terraform validate
 terraform plan
+terraform apply -auto-approve
 
-
+# invoke-webrequest http://localhost:808 -DisableKeepAlive -UseBasicParsing -ErrorAction SilentlyContinue
