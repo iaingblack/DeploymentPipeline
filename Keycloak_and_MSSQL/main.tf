450345edd81ebd3e8017efcce335176bb76fd7e8
Iain Black <iain@rootisgod.xom>
2021-07-04 07:54:53 +0100

    Getting good. Need csv to create users from

--- Keycloak_and_MSSQL/main.tf
+++ Keycloak_and_MSSQL/main.tf
@@ -13,3 +13,28 @@ provider "keycloak" {
     password      = "admin123"
     url           = "http://localhost:8080"
 }
+
+resource "keycloak_realm" "test-realm" {
+  realm   = "test-realm"
+  enabled = true
+}
+
+resource "keycloak_user" "user_with_initial_password" {
+  realm_id   = keycloak_realm.test-realm.id
+  username   = "alice"
+  enabled    = true
+
+  email      = "alice@domain.com"
+  first_name = "Alice"
+  last_name  = "Aliceberg"
+
+  attributes = {
+    foo = "bar"
+    multivalue = "value1##value2"
+  }
+
+  initial_password {
+    value     = "Password1"
+    temporary = false
+  }
+}
